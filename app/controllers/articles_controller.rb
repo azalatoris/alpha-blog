f13efe354907420bb887beadc5c9c625d37cc6c7
Algirdas Zalatoris <104355658+azalatoris@users.noreply.github.com>
2022-06-01 19:48:56 +0300

    Commit to solve an error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def show
     @article = Article.find(params[:id])
   end
@@ -9,13 +8,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
-
 end
